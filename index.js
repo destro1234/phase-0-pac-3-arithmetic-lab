4100aa6768823ccbb488fc81b1db056f51703304
Elias <eliasventura23@gmail.com>
2022-02-15 13:21:20 -0500

    commit tree d60b2dcb4809355f892b907e2b7205cc2898eb99

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a/b
+}
+
+function increment(n) {
+    n++
+    return n
+}
+
+function decrement(n) {
+    n--
+    return n
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10)
+    return n
+}
+
+function preserveDecimal(n) {
+    n = parseFloat(n)
+    return n
+}
